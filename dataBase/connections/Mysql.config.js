716ecf1431ca527696095c180e354c64aeafec8b
Carolina-Ceron <carol.ceron801@gmail.com>
2024-07-07 22:04:45 -0300

    removed default values of config files

--- dataBase/connections/Mysql.config.js
+++ dataBase/connections/Mysql.config.js
@@ -1,8 +1,8 @@
 const config = {
-  host: process.env.HOST_DB || 'localhost', 
-  user: process.env.USER_DB || 'root',
-  password: process.env.PASS_DB || '',
-  database: process.env.DB || 'himitsubd'
+  host: process.env.HOST_DB, 
+  user: process.env.USER_DB,
+  password: process.env.PASS_DB,
+  database: process.env.DB
 }
 
 export default config
